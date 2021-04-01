7146ad2c680a23095324622f50df93306fad388a
Samuel Kyletoft <skyletoft@gmail.com>
2021-04-01 02:12:54 +0200

    Early web interface work

---
+++ cflisp-web/src/script.js
@@ -0,0 +1,10 @@
+import init, {add} from "./pkg/without_a_bundler.js";
+
+async function run() {
+	await init();
+	const result = add(1, 2);
+	console.log(`1 + 2 = ${result}`);
+	if (result !== 3) throw new Error("wasm addition doesn't work!");
+}
+
+run();
