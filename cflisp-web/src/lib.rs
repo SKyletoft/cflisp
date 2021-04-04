91a4974ef2a9bcb9a25a36acd00ace6d49c58f04
Samuel Kyletoft <skyletoft@gmail.com>
2021-04-04 20:22:17 +0200

    Optimise flag changes updated in web too

--- cflisp-web/src/lib.rs
+++ cflisp-web/src/lib.rs
@@ -1,14 +1,14 @@
 use wasm_bindgen::prelude::*;
 
 use cflisp_lib::{
-	compile_flisp, flags::Flags, optimise_flisp, parser, structless::LanguageElementStructless,
-	text,
+	compile_flisp, flags::Flags, optimise_flisp, optimise_language, parser,
+	structless::LanguageElementStructless, text,
 };
 
 #[wasm_bindgen]
 pub fn run(
 	source: &str,
-	optimise: bool,
+	optimise: u8,
 	type_check: bool,
 	debug: bool,
 	hex: bool,
@@ -31,7 +31,7 @@ pub fn run(
 
 fn compile_to_text(
 	source: &str,
-	optimise: bool,
+	optimise: u8,
 	type_check: bool,
 	debug: bool,
 	hex: bool,
@@ -39,25 +39,30 @@ fn compile_to_text(
 	show_imports: bool,
 ) -> Result<String, String> {
 	let flags = Flags {
-		hex: hex,
-		comments: comments,
+		hex,
+		comments,
 		tree: false,
 		tree_structless: false,
-		type_check: type_check,
+		type_check,
 		print_result: false,
 		assemble: false,
 		out: String::new(),
-		debug: debug,
-		optimise: optimise,
+		debug,
+		optimise: optimise.min(2).max(0),
 		inline: false,
 	};
 	let stripped_comments = parser::remove_comments(source);
 	let tree =
 		parser::parse(&stripped_comments, flags.debug).map_err(|err| format!("{:?}", err))?;
-	let structless = LanguageElementStructless::from_language_elements(tree)
+	let mut structless = LanguageElementStructless::from_language_elements(tree)
 		.map_err(|err| format!("{:?}", err))?;
-	let mut inst = compile_flisp::compile(&structless, &flags).map_err(|err| format!("{:?}", err))?;
-	if optimise {
+	if optimise >= 2 {
+		optimise_language::all_optimisations(&mut structless)
+			.map_err(|err| format!("{:?}", err))?;
+	}
+	let mut inst =
+		compile_flisp::compile(&structless, &flags).map_err(|err| format!("{:?}", err))?;
+	if optimise >= 1 {
 		optimise_flisp::all_optimisations(&mut inst).map_err(|err| format!("{:?}", err))?;
 	}
 	let mut text = text::instructions_to_text(&inst, &flags).map_err(|err| format!("{:?}", err))?;
