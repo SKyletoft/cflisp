44d269cca4fff2e188c8dc1db9f24331c50d9d48
Samuel Kyletoft <skyletoft@gmail.com>
2021-04-05 14:20:01 +0200

    cflisp-web: auto recompile on changes

--- cflisp-web/src/script.js
+++ cflisp-web/src/script.js
@@ -1,22 +1,37 @@
 import init, {add, run_cc} from "../pkg/cflisp_web.js";
 
+let last_state;
+
 async function run() {
 	await init();
 	if (add(1, 2) !== 3) throw new Error("wasm doesn't work!");
 
 	function compile_and_write() {
-		const source                            = document.getElementById("source").value;
-		const opt                               = document.getElementById("opt").value;
-		const type_check                        = document.getElementById("type_check").checked;
-		const debug                             = document.getElementById("debug").checked;
-		const hex                               = document.getElementById("hex").checked;
-		const comments                          = document.getElementById("comments").checked;
-		const imports                           = document.getElementById("imports").checked;
-		const result                            = run_cc(source, opt, type_check, debug, hex, comments, imports);
-		document.getElementById("result").value = result;
+		const source     = document.getElementById("source").value;
+		const opt        = document.getElementById("opt").value;
+		const type_check = document.getElementById("type_check").checked;
+		const debug      = document.getElementById("debug").checked;
+		const hex        = document.getElementById("hex").checked;
+		const comments   = document.getElementById("comments").checked;
+		const imports    = document.getElementById("imports").checked;
+
+		const state = {
+			source : source,
+			opt : opt,
+			type_check : type_check,
+			debug : debug,
+			hex : hex,
+			comments : comments,
+			imports : imports
+		};
+
+		if (state != last_state) {
+			document.getElementById("result").value = run_cc(source, opt, type_check, debug, hex, comments, imports);
+		}
+		last_state = state;
 	}
 
-	document.getElementById("run_btn").addEventListener("click", compile_and_write);
+	setInterval(compile_and_write, 250);
 }
 
 run();
