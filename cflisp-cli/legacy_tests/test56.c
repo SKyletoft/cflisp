e5fcf488c2fa7f2cb93db67ac2d994d7693eaf6e
Samuel Kyletoft <skyletoft@gmail.com>
2021-08-22 03:36:51 +0200

    IO tests

---
+++ cflisp-cli/legacy_tests/test56.c
@@ -0,0 +1,10 @@
+// test56.c
+
+#include <stdio.h>
+
+const char *string        = "Hello world";
+const char *dont_get_read = "hi again";
+
+void main() {
+	print(string);
+}
---
+++ cflisp-cli/legacy_tests/test57.c
@@ -0,0 +1,8 @@
+// test57.c
+#include <stdio.h>
+
+static char buffer[32];
+
+int main() {
+	int chars = read_str(buffer);
+}
